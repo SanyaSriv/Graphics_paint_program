9ea54f6fdb8193b2d509943110e0346ecdf9b19f
SanyaSriv <ssrivas7@ucsc.edu>
2022-10-07 20:33:43 -0700

    Added render function to point class

--- asg1.js
+++ asg1.js
@@ -35,6 +35,20 @@ class Point {
     this.type = "point";
     this.size = 5.0;
   }
+
+  render() {
+    var xy = this.position;
+    var rgba = this.color;
+    var size = this.size;
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    // passing the size too now
+    gl.uniform1f(u_PointSize, size);
+    // Draw
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
 
 
@@ -99,17 +113,7 @@ function renderAllShapes() {
 
   var len = g_points_array.length;
   for(var i = 0; i < len; i++) {
-    var xy = g_points_array[i].position;
-    var rgba = g_points_array[i].color;
-    var size = g_points_array[i].size;
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // passing the size too now
-    gl.uniform1f(u_PointSize, size);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
+    g_points_array[i].render();
   }
 }
 
